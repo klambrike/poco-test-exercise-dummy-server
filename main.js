9e8635847ba0dbdf0fa793dae344050d9d1f9a19
Andres Kukk <andres@pocopay.com>
2018-04-11 14:52:16 +0300

    Add page for testing javascript alerts

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ app.set('port', (process.env.PORT || 8081));
 
 app.use(bodyParser.json())
 
+app.use('/test', express.static('public'));
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
@@ -17,7 +19,7 @@ app.get('/listUsers', function (req, res) {
 app.post('/addUser', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log('users: ' + data);
-       
+
        // Mock server timeout so candidate would be forced to build request repeating mechanism into the app
        serverConnectionIsUp = Math.random() > 0.5;
        if(!serverConnectionIsUp) {
@@ -25,7 +27,7 @@ app.post('/addUser', function (req, res) {
        }
        else {
            data = JSON.parse( data );
-       
+
            if (req.body == null || req.body.email == null || req.body.password == null
                    || req.body.country == null || req.body.city == null || req.body.postal_code == null) {
                res.status(400).json({ error: 'err.missing.params' })
@@ -34,16 +36,16 @@ app.post('/addUser', function (req, res) {
                res.status(400).json({ error: 'err.password.too.short' })
            }
            else {
-               userExists = data[req.body.email]; 
-            
+               userExists = data[req.body.email];
+
                if (userExists != null) {
                    console.log('user exists');
-                   res.status(400).json({ error: 'err.user.exists' })  
+                   res.status(400).json({ error: 'err.user.exists' })
                }
                else {
                    console.log('user does not exist');
                    data[req.body.email] = req.body;
-                        
+
                    fs.writeFile(__dirname + "/" + "users.json", JSON.stringify(data), function(err) {
                        if(err) {
                            console.log(err);
@@ -51,7 +53,7 @@ app.post('/addUser', function (req, res) {
                            console.log("JSON saved");
                        }
                    });
-                            
+
                    res.status(200).json({ data: 'success' })
                }
            }
@@ -62,17 +64,17 @@ app.post('/addUser', function (req, res) {
 app.post('/login', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
-       
+
        data = JSON.parse( data );
-       
+
        if (req.body == null || req.body.email == null || req.body.password == null) {
            res.status(400).json({ error: 'err.missing.params' })
        }
        else {
            userExists = data[req.body.email];
-           
+
            if (userExists == null ||  userExists.password != req.body.password) {
-                res.status(400).json({ error: 'err.wrong.credentials' })  
+                res.status(400).json({ error: 'err.wrong.credentials' })
            }
            else {
                res.status(200).json({ data: 'success' })
